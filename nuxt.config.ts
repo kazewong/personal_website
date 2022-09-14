58448e5f7930ca36d5e39c32754d236c3c5d7a5a
kazewong <kazewong.physics@gmail.com>
2022-09-14 12:43:43 -0400

    vuetify seems to work fine now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    css: ['vuetify/lib/styles/main.sass'],
+    build: {
+      transpile: ['vuetify'],
+    },
     modules: [
         '@nuxt/content',
         '@nuxtjs/tailwindcss'
