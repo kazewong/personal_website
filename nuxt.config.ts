efb137177d79f7fb65c3729ab86026e8bc7b3a3d
Kaze Wong <kazewong.physics@gmail.com>
2022-09-13 22:46:38 -0400

    Add content and tailwind dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    modules: [
+        '@nuxt/content',
+        '@nuxtjs/tailwindcss'
+      ],
+      content: {
+        // https://content.nuxtjs.org/api/configuration
+      }
 })
