e3c00da37c6740924a562b11c523a2be253a84f1
Kaze Wong <kazewong.physics@gmail.com>
2022-09-15 00:00:55 -0400

    start making header

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
-        
+    <div class="p-2">
+        <nav>
+            <div class="flex flex-row ">
+                <NuxtLink class="basis-1/2 bg-indigo-500 rounded" to="/">Home page</NuxtLink>
+                <NuxtLink to="/about">About</NuxtLink>
+            </div>
+        </nav>
     </div>
 </template>
