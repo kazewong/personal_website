fd5ba3fe380f3e5c5d937e2f2a03ecc34798f781
kazewong <kazewong.physics@gmail.com>
2022-09-15 18:35:39 -0400

    update

--- components/Header.vue
+++ components/Header.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="p-2">
-        <nav>
-            <div class="flex flex-row ">
-                <NuxtLink class="basis-1/2 bg-indigo-500 rounded" to="/">Home page</NuxtLink>
-                <NuxtLink to="/about">About</NuxtLink>
-            </div>
-        </nav>
+
+    <div class="container bg-indigo-500 rounded min-w-min">
+        <div class="flex flex-row ">
+            <NuxtLink class="w-1/5 center" to="/">About</NuxtLink>
+            <NuxtLink class="w-1/5 center" to="/science">Science</NuxtLink>
+            <NuxtLink class="w-1/5" to="/art">Art</NuxtLink>
+            <NuxtLink class="w-1/5" to="/highjump">Jump</NuxtLink>
+            <NuxtLink class="w-1/5" to="/blog">Blog</NuxtLink>
+        </div>
     </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
+    <div class="absolute top-0 right-0">
+        <div>
+            <h1>Hi</h1>
+        </div>
         <Header />
         <slot />
     </div>
