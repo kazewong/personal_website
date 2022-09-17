275547ac0f6b71c6d3abd5a92cb2c7ba96a56996
Kaze Wong <kazewong.physics@gmail.com>
2022-09-17 12:30:00 -0400

    Add dropdown menu component with input

--- components/Header.vue
+++ components/Header.vue
@@ -4,8 +4,8 @@
 			<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 				<nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
 					<!-- Header buttons -->
-					<DropdownMenu />
-					<DropdownMenu />
+					<DropdownMenu name="About" linkList=[{message:'Foo'}]/>
+					<DropdownMenu name="Science"/>
 					<!-- <NuxtLink class="mr-5 hover:text-gray-900" to="/">About</NuxtLink>
 					<NuxtLink class="mr-5 hover:text-gray-900" to="/science">Science</NuxtLink>
 					<NuxtLink class="mr-5 hover:text-gray-900" to="/personal">Personal</NuxtLink>
