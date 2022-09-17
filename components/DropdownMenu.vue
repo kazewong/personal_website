ec9c8c998c9375bcf95ab54beda0e5a8493768d2
kazewong <kazewong.physics@gmail.com>
2022-09-17 17:16:00 -0400

    Update dropdown menu

--- components/DropdownMenu.vue
+++ components/DropdownMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="mr-5 hover:text-gray-900" @click="toggleDropdown">
+    <div class="mr-5 hover:text-indigo-700" @click="toggleDropdown">
         <h1>
             {{ name }}
         </h1>
@@ -12,21 +12,15 @@
         x-transition:leave="transition ease-in duration-100"
         x-transition:leave-start="opacity-100 scale-100"
         x-transition:leave-end="opacity-0 scale-90" 
-        class="absolute z-20 w-48 py-2 mt-2 bg-white rounded-md shadow-xl dark:bg-gray-800">
-        <li v-for="link in linkList">
-            <NuxtLink :to=link.dir> {{ link.name }} </NuxtLink>
+        class="absolute z-20 w-24 py-2 mt-2 bg-gray-200 rounded-md shadow-xl dark:bg-gray-800">
+        <li v-for="link in linkList" class="list-none">
+            <NuxtLink class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white" :to=link.dir> {{ link.name }} </NuxtLink>
         </li>
-            <!-- <a href="#" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> your profile </a>
-            <a href="#" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> Your projects </a>
-            <a href="#" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> Help </a>
-            <a href="#" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> Settings </a>
-            <a href="#" class="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white"> Sign Out </a> -->
         </div>
     </div>
 </template>
 
 <script>
-
 export default{
     props: {
         name: String,
@@ -41,9 +35,10 @@ export default{
         toggleDropdown(){
             this.isOpen = !this.isOpen
         },
-        setDropdownFalse(){
-            this.isOpen = false
+        setDropdownFalse() {
+            setTimeout(() => this.isOpen = false, 500);
         }
     }
 }
 </script>
+
--- components/Header.vue
+++ components/Header.vue
@@ -4,15 +4,23 @@
 			<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 				<nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
 					<!-- Header buttons -->
-                    <DropdownMenu name="About" :linkList="[{dir:'blog' , name:'Blog'}]"></DropdownMenu>
-					<DropdownMenu name="Science"/>
-					<!-- <NuxtLink class="mr-5 hover:text-gray-900" to="/">About</NuxtLink>
-					<NuxtLink class="mr-5 hover:text-gray-900" to="/science">Science</NuxtLink>
-					<NuxtLink class="mr-5 hover:text-gray-900" to="/personal">Personal</NuxtLink>
-					<NuxtLink class="mr-5 hover:text-gray-900" to="/blog">Blog</NuxtLink>					 -->
+                    <DropdownMenu name="About" class="px-10"></DropdownMenu>
+					<DropdownMenu name="Science" :linkList="scienceList" />
 				</nav>
 			</div>
 		</header>
     </div>
 </template>
 
+<script>
+export default{
+    data(){
+        return {
+            scienceList: [
+            {dir:'/Science/blog' , name:'Blog'},
+            {dir:'/Science/summary', name:'Summary'}
+        ]
+        }
+    }
+}
+</script>
